6480af2462e58931c81d9a60b68c01d3aa65e6f7
OMEGAlul111 <58611439+OMEGAlul111@users.noreply.github.com>
2021-11-27 02:28:43 +0300

    views added

--- core/views.py
+++ core/views.py
@@ -1,2 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+def profile(request):
+    return render(request, 'profile.html', {})
+
+
+def login_page(request):
+    return render(request, 'login_page.html', {})
+
+
+def register_page(request):
+    return render(request, 'register_page.html', {})
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html', {})
+
+
+def deadlines(request):
+    return render(request, 'deadlines.html', {})
+
+
+def tasks(request):
+    return render(request, 'tasks.html', {})
+
+
+def solutions(request):
+    return render(request, 'solutions.html', {})
+
+
+def solution_page(request):
+    return render(request, 'solution_page.html', {})
+
+
+def deadline_page(request):
+    return render(request, 'deadline_page.html',{})
+
+
+def task_page(request):
+    return render(request, 'task_page.html', {})
+
+
+def group_page(request):
+    return render(request, 'group_page.html', {})
+
+
+def person_page(request):
+    return render(request, 'person_page.html', {})
+
+
+def login_page(request):
+    return render(request, 'login_page.html', {})
+
+
+def create_task(request):
+    return render(request, 'create_task.html', {})
+
+
+def give_task(request):
+    return render(request, 'give_task.html', {})
