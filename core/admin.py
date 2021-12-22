5dc422287a9550ffc0cc271f6a740bd2259a5dae
Egogorn <egogorn@gmail.com>
2021-12-22 23:02:27 +0300

    database

--- core/admin.py
+++ core/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
-from .models import StudentGroup, Task, Profile
+from .models import StudentGroup, Task, Profile, Solution
 # Register your models here.
 
 #admin.site.register(Profile)
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('name', 'surname', 'group', 'is_teacher')
+    list_display = ('name', 'surname', 'is_teacher')
 
 admin.site.register(Profile, ProfileAdmin)
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'deadline')
     pass
 
 @admin.register(StudentGroup)
 class StudentGroupAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Solution)
+class SolutionAdmin(admin.ModelAdmin):
+    list_display = ('task', 'student', 'time')
     pass
--- core/models.py
+++ core/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 
 class StudentGroup(models.Model):
     name = models.CharField(default="", max_length=5)
-
     def __str__(self):
         return self.name
 
@@ -15,7 +14,8 @@ class Task(models.Model):
     name = models.CharField(max_length=100)
     desc = models.CharField(max_length=400)
     group = models.ManyToManyField(StudentGroup)
-
+    maxPoints = models.IntegerField(default=-1)
+    deadline = models.DateTimeField()
     def __str__(self):
         return self.name
 
@@ -23,12 +23,25 @@ class Task(models.Model):
 class Profile(models.Model):
     name = models.CharField(max_length=50, default="Ivan")
     surname = models.CharField(max_length=50, default="Ivanov")
-    group = models.ForeignKey(StudentGroup, on_delete=models.CASCADE, null=True, blank=True)
+    father_name = models.CharField(max_length=50, default="Ivanovich")
+    group = models.ManyToManyField(StudentGroup, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_teacher = models.BooleanField(default=False)
 
     def __str__(self):
-        return '%s %s' % (self.name, self.surname)
+        if not self.is_teacher:
+            return '%s %s %s' % (self.name, self.surname, self.group.all()[0])
+        else:
+            return '%s %s' % (self.name, self.surname)
+
+class Solution(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.PROTECT)
+    student = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    solution = models.FileField(blank=True)
+    time = models.DateTimeField()
+    file = models.FileField(blank=True)
+    def __str__(self):
+        return 'Решение %s от %s' % (self.task.name, self.student)
 
 
 @receiver(post_save, sender=User)
