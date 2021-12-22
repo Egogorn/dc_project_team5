8f52fdac5e5dade5fa55999be160a6eac81324e7
Romkashka <halilov.g.2020@gmail.com>
2021-12-23 01:02:38 +0300

    tests: replace some tests into FutureURLTest

--- core/tests.py
+++ core/tests.py
@@ -2,23 +2,70 @@ from django.test import TestCase
 
 # Create your tests here.
 
+
 class URLSTests(TestCase):
-    def test_testFakePage(self):
+    def test_fake_page(self):
         response = self.client.get('/fake')
         self.assertEqual(response.status_code, 404)
 
-    def test_testProfilePage(self):
+    def test_profile_page(self):
         response = self.client.get('/profile')
         self.assertEqual(response.status_code, 200)
 
-    def test_testLoginPage(self):
+    def test_dashboard_page(self):
+        response = self.client.get('/dashboard')
+        self.assertEqual(response.status_code, 200)
+
+
+class FutureURLTest(TestCase):
+    def test_login_page(self):
         response = self.client.get('/login_page')
         self.assertEqual(response.status_code, 200)
 
-    def test_testRegisterPage(self):
+    def test_register_page(self):
         response = self.client.get('/register_page')
         self.assertEqual(response.status_code, 200)
 
-    def test_testDashboardPage(self):
-        response = self.client.get('/dashboard')
+    def test_crete_task_page(self):
+        response = self.client.get('/create_task')
+        self.assertEqual(response.status_code, 200);
+
+    def test_deadlines_page(self):
+        response = self.client.get('/deadlines')
+        self.assertEqual(response.status_code, 200)
+
+    def test_deadline_page(self):
+        response = self.client.get('/deadline')
+        self.assertEqual(response.status_code, 200)
+
+    def test_tasks_page(self):
+        response = self.client.get('/tasks')
+        self.assertEqual(response.status_code, 200)
+
+    def test_solutions_page(self):
+        response = self.client.get('/solutions')
+        self.assertEqual(response.status_code, 200)
+
+    def test__solution_page(self):
+        response = self.client.get('/solution_page')
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_page(self):
+        response = self.client.get('/task_page')
+        self.assertEqual(response.status_code, 200)
+
+    def test_group_page(self):
+        response = self.client.get('/group_page')
+        self.assertEqual(response.status_code, 200)
+
+    def test_person_page(self):
+        response = self.client.get('/person_page')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_task_page(self):
+        response = self.client.get('/create_task')
+        self.assertEqual(response.status_code, 200)
+
+    def test_give_task_page(self):
+        response = self.client.get('/give_task')
         self.assertEqual(response.status_code, 200)
